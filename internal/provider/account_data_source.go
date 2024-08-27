b47feae7a16cb4fa8e70481b4b74397efe7bde12
Nayyara Cropsey <nayyara@greynoise.io>
2024-08-27 09:58:11 -0400

    Adds account data source

---
+++ internal/provider/account_data_source.go
@@ -0,0 +1,82 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+var _ datasource.DataSource = &AccountDataSource{}
+
+func NewAccountDataSource() datasource.DataSource {
+	return &AccountDataSource{}
+}
+
+type AccountDataSource struct {
+	data *Data
+}
+
+type AccountDataSourceModel struct {
+	UserID      types.String `tfsdk:"user_id"`
+	WorkspaceID types.String `tfsdk:"workspace_id"`
+}
+
+func (d *AccountDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_account"
+}
+
+func (d *AccountDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Account data source",
+		Attributes: map[string]schema.Attribute{
+			"user_id": schema.StringAttribute{
+				MarkdownDescription: "User ID of the account",
+				Computed:            true,
+			},
+			"workspace_id": schema.StringAttribute{
+				MarkdownDescription: "Workspace ID of the account",
+				Computed:            true,
+			},
+		},
+	}
+}
+
+func (d *AccountDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	data, ok := req.ProviderData.(*Data)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("expected *Data, got: %T. "+
+				"Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.data = data
+}
+
+func (d *AccountDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var config AccountDataSourceModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	config.UserID = types.StringValue(d.data.Client.UserID().String())
+	config.WorkspaceID = types.StringValue(d.data.Client.WorkspaceID().String())
+
+	tflog.Trace(ctx, "read account data source")
+	resp.Diagnostics.Append(resp.State.Set(ctx, &config)...)
+}
